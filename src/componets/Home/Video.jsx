71421f02efaa4441f7857de8812d9f2a53849740
MR-deepaksharma <sharmadeepak5827@gmail.com>
2025-10-18 22:41:29 +0530

    Add className to img in Video component

--- src/componets/Home/Video.jsx
+++ src/componets/Home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div>
-   <img src="https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500"></img>
+   <img className="h-full w-full" src="https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGltYWdlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500"></img>
     </div>
   )
 }
